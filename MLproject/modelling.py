46983c47e4f69aa10b0593530164ed4b0a633cf4
NajwaKurnia <kurnianajea8@gmail.com>
2026-05-27 19:00:08 +0700

    Fix: dagshub auth token dari env

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,7 +11,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
 
-# Argparse 
+# Argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_estimators", type=int, default=100)
 parser.add_argument("--max_depth", type=int, default=10)
@@ -18,6 +19,10 @@ parser.add_argument("--test_size", type=float, default=0.2)
 args = parser.parse_args()
 
 # Setup DagsHub
+token = os.environ.get("DAGSHUB_USER_TOKEN", "")
+os.environ["MLFLOW_TRACKING_USERNAME"] = token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = token
+
 dagshub.init(
     repo_owner="NajwaKurnia",
     repo_name="Ekperimen_SML_Najwa-Kurnia",
